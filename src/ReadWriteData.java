b14f550a9756ac64fe2fda9da3709dd7f7eb8074
Kevin M <crisstian2345@gmail.com>
2023-07-20 06:10:18 -0500

    add jar

--- src/ReadWriteData.java
+++ src/ReadWriteData.java
@@ -15,93 +15,98 @@ import javax.swing.JTextArea;
 
 public class ReadWriteData {
 	private String lastDataTime = "";
-	
+
 	public void writeData(String tag, String description) {
 		int limit = 5;
 		DateTime dateTime = new DateTime();
 		LocalDateTime now = LocalDateTime.now();
 		String format = "Date: %s;Time: %s;Tag: %s;Description: %s";
-		
+
 		String newDescription = description.replace("\n", "\\n");
 		String line = String.format(format, dateTime.getDate(now), dateTime.getTime(now), tag, newDescription);
-		
+
 		try {
-			Path[] paths = {Paths.get("./Data/data.csv"), Paths.get("./Data/history.csv")};
-			
-			if(!Files.exists(paths[0]))
+			Path[] paths = { Paths.get("./Data/data.csv"), Paths.get("./Data/history.csv") };
+
+			if (!Files.exists(paths[0]))
 				throw new FileNotFoundException("The file data.csv doesnt exist in the directory Data");
 			else if (!Files.exists(paths[1]))
 				throw new FileNotFoundException("The file history.csv doesnt exist in the directory Data");
-			
+
 			for (Path path : paths) {
-				BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8, StandardOpenOption.APPEND);
+				BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8,
+						StandardOpenOption.APPEND);
 				writer.newLine();
 				writer.write(line);
 				writer.close();
 			}
 
-		    Path dataCsvPath = Paths.get("./Data/data.csv");
-		    List<String> lines = Files.readAllLines(dataCsvPath);
-		    lines.removeIf(String::isEmpty);
-		    
-		    if (lines.size() > limit) {
-		        List<String> updatedLines = lines.subList(lines.size() - limit, lines.size());
-		        Files.write(dataCsvPath, updatedLines, StandardCharsets.UTF_8);
-		    }
+			Path dataCsvPath = Paths.get("./Data/data.csv");
+			List<String> lines = Files.readAllLines(dataCsvPath);
+			lines.removeIf(String::isEmpty);
+
+			if (lines.size() > limit) {
+				List<String> updatedLines = lines.subList(lines.size() - limit, lines.size());
+				Files.write(dataCsvPath, updatedLines, StandardCharsets.UTF_8);
+			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	public void setLastData(JTextArea out) {
 		String info = readLastLine();
 		out.setText(info);
 	}
-	
+
 	public void setLblDescription(JLabel out) {
 		DateTime dateTime = new DateTime();
 		LocalDateTime now = LocalDateTime.now();
 		String format = "Description(%s - %s):";
-		String splited = lastDataTime.split(" ")[1];
-		
-		String info = String.format(format, splited, dateTime.getTime(now));
+		String splited = "";
+		String info = "";
+		if (!lastDataTime.isEmpty()) {
+			splited = lastDataTime.split(" ")[1];
+			info = String.format(format, splited, dateTime.getTime(now));
+		} else
+			info = String.format(format, "0", "0");
 		out.setText(info);
 	}
-	
+
 	private String readLastLine() {
 		String info = "";
-		
+
 		try {
 			Path filePath = Paths.get("./Data/data.csv");
-			if(!Files.exists(filePath))
+			if (!Files.exists(filePath))
 				throw new FileNotFoundException("The file data.csv doesnt exist in the directory Data");
 
 			List<String> lines = Files.readAllLines(filePath);
 
 			if (!lines.isEmpty()) {
-				//to get information of the last line in data.csv
+				// to get information of the last line in data.csv
 				String lastLine = lines.get(lines.size() - 1);
 				String[] lineSplited = lastLine.split(";");
 				String tag = "";
 				String description = "";
-				
+
 				for (String part : lineSplited) {
-					if(part.contains("Tag:"))
+					if (part.contains("Tag:"))
 						tag = part;
-					else if(part.contains("Description:"))
+					else if (part.contains("Description:"))
 						description = part;
-					else if(part.contains("Time:"))
+					else if (part.contains("Time:"))
 						lastDataTime = part;
 				}
-				
+
 				info = tag + " - " + description;
 			} else
 				info = "History clean";
-			
+
 		} catch (Exception e) {
 			System.out.println("An error occurred while reading the file: " + e.getMessage());
 		}
-		
+
 		return info;
 	}
 }
--- src/ShortcutManager.java
+++ src/ShortcutManager.java
@@ -9,20 +9,20 @@ import javax.swing.JTextArea;
 import javax.swing.KeyStroke;
 
 public class ShortcutManager {
-	
-	public void add(JTextArea textArea, Popup popup) {
-		
-		@SuppressWarnings("serial")
-		Action saveAction = new AbstractAction("Save") {
+
+    public void add(JTextArea textArea, Popup popup) {
+
+        @SuppressWarnings("serial")
+        Action saveAction = new AbstractAction("Save") {
             @Override
             public void actionPerformed(ActionEvent e) {
-            	popup.SaveClose();
+                popup.SaveClose();
             }
         };
 
         // Add the action to the txtDescription's input map with the Ctrl + S key stroke
         textArea.getInputMap().put(KeyStroke.getKeyStroke(KeyEvent.VK_S, KeyEvent.CTRL_DOWN_MASK), "saveAction");
         textArea.getActionMap().put("saveAction", saveAction);
-		
-	}
+
+    }
 }
